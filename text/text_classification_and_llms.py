db09628779b5ee9c2ca9858d395ff37bf28d3c43
Demian Wassermann <demian.wassermann@inria.fr>
2026-06-26 11:59:37 +0200

    text: use --only-binary instead of --prefer-wheel (works on any pip version)

--- text/text_classification_and_llms.py
+++ text/text_classification_and_llms.py
@@ -15,7 +15,7 @@
 # %%
 
 # %%
-%pip install -q --prefer-wheel git+https://github.com/demianw/Xed.git
+%pip install -q --only-binary llama-cpp-python git+https://github.com/demianw/Xed.git
 
 # %% [markdown]
 # # Text Classification and LLMs: From TF-IDF to Zero-Shot Prompting
